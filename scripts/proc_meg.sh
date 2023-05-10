5f44ef0164d289b22f368153626a27f5d966175e
Price Withers <price.withers@nih.gov>
2023-05-10 14:09:18 -0400

    Add ability to re-run MEG conversion

--- scripts/proc_meg.sh
+++ scripts/proc_meg.sh
@@ -60,7 +60,8 @@ else
 fi
 
 ## PROCESS MEG SCANS
-subj_session_meg_dir=$bids_root/sub-${subj}/ses-meg/meg
+subj_session_meg_dir=$bids_root/sub-${subj}/ses-meg
+subj_meg_dir=$subj_session_meg_dir/meg
 subj_fs_dir=$fs_dir/sub-${subj}_ses-clinical${ses_suffix}
 
 if [ ! -d $subj_fs_dir ]; then
@@ -72,38 +73,53 @@ if [ ! -d $subj_fs_dir ]; then
 fi
 
 fs_subj=${subj_fs_dir##*/}
-
-if [ ! -f $subj_session_meg_dir/sub-${subj}_ses-meg_task-resteyesclosed_run-01_meg.fif ]; then
     
-    # copy all .ds into meg folder
-    python $scripts_dir/retrieve_meg.py "$subj"
-    
-    # retrieve emptyroom and convert to bids format
-    python $scripts_dir/retrieve_emptyroom.py \
-        --pnum "$subj"  \
-        --files_dir "$files_dir"
-    
-    # if subject does not have bem files, then run mne_watershed_bem
-    if [ ! -f "$subj_fs_dir/bem/inner_skull.surf" ]; then
-        mne watershed_bem \
-            -d $fs_dir \
-            -s "$fs_subj"
-    else
-        echo -e "\033[1;33m ++ Bem surfaces already exist for ${subj} ++\033[0m"
-    fi
+# copy all .ds into meg folder
+python $scripts_dir/retrieve_meg.py "$subj"
 
-    # this line is necessary to allow running afni GUI from python script
-    export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:/opt/X11/lib/flat_namespace
+# retrieve emptyroom and convert to bids format
+python $scripts_dir/retrieve_emptyroom.py \
+    --pnum "$subj"  \
+    --files_dir "$files_dir"
 
-    # create .trans file
-    python $scripts_dir/create_trans_meg.py \
-        --fs_subj "$fs_subj" \
-        --pnum "$subj"
+# if subject does not have bem files, then run mne_watershed_bem
+if [ ! -f "$subj_fs_dir/bem/inner_skull.surf" ]; then
+    mne watershed_bem \
+        -d $fs_dir \
+        -s "$fs_subj"
+else
+    echo -e "\033[1;33m ++ Bem surfaces already exist for ${subj} ++\033[0m"
+fi
 
+# this line is necessary to allow running afni GUI from python script
+export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:/opt/X11/lib/flat_namespace
+
+# create .trans file
+python $scripts_dir/create_trans_meg.py \
+    --fs_subj "$fs_subj" \
+    --pnum "$subj"
+
+if [ ! -f "$subj_meg_dir"/sub-"${subj}"_ses-meg_task-resteyesclosed_run-01_meg.fif ]; then
     # convert meg to bids format
     python $scripts_dir/convert_meg.py \
         --fs_subj "$fs_subj" \
         --pnum "$subj"  \
         --files_dir "$files_dir"
+else
+    echo -e "\033[0;35m++ Do you want to delete and re-convert all MEG .ds to fif? Enter y if yes, n if not. ++\033[0m"
+    read -r ynresponse
+    ynresponse=$(echo "$ynresponse" | tr '[:upper:]' '[:lower:]')
 
+    if [ "$ynresponse" == "y" ]; then
+
+        rm -rf "$subj_session_meg_dir"
+
+        # convert meg to bids format
+        python $scripts_dir/convert_meg.py \
+            --fs_subj "$fs_subj" \
+            --pnum "$subj"  \
+            --files_dir "$files_dir"
+    else
+        exit 1
+    fi
 fi
