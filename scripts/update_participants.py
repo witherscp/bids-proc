12b85a33cc77d52fd7c4b02a46860cd689345768
Price Withers <price.withers@nih.gov>
2023-05-11 12:06:55 -0400

    Remove megpostop and re-check old dirs

--- scripts/update_participants.py
+++ scripts/update_participants.py
@@ -16,18 +16,18 @@ from colors import Colors
 neu_dir = Path("/Volumes/shares/NEU")
 bids_root = neu_dir / 'Data'
 
-def add_session_to_dict(session, subj_dict):
+def add_session_to_dict(session, new_dict, old_dict):
     
-    if subj_dict[session] == [0]:
-        subj_dict[session] = [1]
-        
+    new_dict[session] = [1]
+    
+    if old_dict[session] == [0]:
         print(
             Colors.GREEN,
             f'Adding {session} to participants.tsv for {pnum}',
             Colors.END
         )
         
-    return subj_dict
+    return new_dict
     
 
 if __name__ == "__main__":
@@ -52,49 +52,48 @@ if __name__ == "__main__":
         keep_default_na=False
     )
     
+    new_dict = {
+        'participant_id':[f'sub-{pnum}'],
+        'sex':['n/a'],
+        'handedness':['n/a'],
+        'ses-clinical':[0],
+        'ses-clinicalpostop':[0],
+        'ses-research':[0],
+        'ses-research_anat-t2fatsat':[0],
+        'ses-research_dwi':[0],
+        'ses-research_perf':[0],
+        'ses-research_task-resteyesopen':[0],
+        'ses-research_task-resteyesopen_physio':[0],
+        'ses-research_task-resteyesclosed':[0],
+        'ses-research_task-resteyesclosed_physio':[0],
+        'ses-researchpostop':[0],
+        'ses-researchpostop_anat-t2fatsat':[0],
+        'ses-researchpostop_dwi':[0],
+        'ses-researchpostop_perf':[0],
+        'ses-researchpostop_task-resteyesopen':[0],
+        'ses-researchpostop_task-resteyesopen_physio':[0],
+        'ses-researchpostop_task-resteyesclosed':[0],
+        'ses-researchpostop_task-resteyesclosed_physio':[0],
+        'ses-meg':[0],
+        'ses-meg_task-resteyesopen':[0],
+        'ses-meg_task-resteyesclosed':[0],
+        'ses-altclinical':[0],
+        'ses-altclinical_anat-t1':[0],
+        'ses-altclinical_anat-t2':[0],
+        'ses-altclinical_anat-flair':[0],
+        'ses-altclinicalpostop':[0],
+        'ses-altclinicalpostop_anat-t1':[0],
+        'ses-altclinicalpostop_anat-t2':[0],
+        'ses-altclinicalpostop_anat-flair':[0],
+    }
+    
     if f'sub-{pnum}' in df.participant_id.unique():
         is_new_subject = False
         subj_df = df.loc[df.participant_id == f'sub-{pnum}']
-        subj_dict = subj_df.to_dict('list')
+        old_dict = subj_df.to_dict('list')
     else:
         is_new_subject = True
-        subj_dict = {
-            'participant_id':[f'sub-{pnum}'],
-            'sex':['n/a'],
-            'handedness':['n/a'],
-            'ses-clinical':[0],
-            'ses-clinicalpostop':[0],
-            'ses-research':[0],
-            'ses-research_anat-t2fatsat':[0],
-            'ses-research_dwi':[0],
-            'ses-research_perf':[0],
-            'ses-research_task-resteyesopen':[0],
-            'ses-research_task-resteyesopen_physio':[0],
-            'ses-research_task-resteyesclosed':[0],
-            'ses-research_task-resteyesclosed_physio':[0],
-            'ses-researchpostop':[0],
-            'ses-researchpostop_anat-t2fatsat':[0],
-            'ses-researchpostop_dwi':[0],
-            'ses-researchpostop_perf':[0],
-            'ses-researchpostop_task-resteyesopen':[0],
-            'ses-researchpostop_task-resteyesopen_physio':[0],
-            'ses-researchpostop_task-resteyesclosed':[0],
-            'ses-researchpostop_task-resteyesclosed_physio':[0],
-            'ses-meg':[0],
-            'ses-meg_task-resteyesopen':[0],
-            'ses-meg_task-resteyesclosed':[0],
-            'ses-megpostop':[0],
-            'ses-megpostop_task-resteyesopen':[0],
-            'ses-megpostop_task-resteyesclosed':[0],
-            'ses-altclinical':[0],
-            'ses-altclinical_anat-t1':[0],
-            'ses-altclinical_anat-t2':[0],
-            'ses-altclinical_anat-flair':[0],
-            'ses-altclinicalpostop':[0],
-            'ses-altclinicalpostop_anat-t1':[0],
-            'ses-altclinicalpostop_anat-t2':[0],
-            'ses-altclinicalpostop_anat-flair':[0],
-        }
+        old_dict = new_dict.copy()
     
     val_options_dict = {
         'sex': [('M','F','n/a'),'M, F, n/a if unknown'],
@@ -107,14 +106,18 @@ if __name__ == "__main__":
     for ses_path in subj_path.glob('ses-*'):
         session = ses_path.stem
         
-        if session not in subj_dict.keys():
+        if session not in new_dict.keys():
             continue
         
-        subj_dict = add_session_to_dict(session=session,subj_dict=subj_dict)
+        new_dict = add_session_to_dict(
+            session=session,
+            new_dict=new_dict,
+            old_dict=old_dict
+        )
     
     for ses_suffix in ('', 'postop'):
         
-        if subj_dict[('ses-research' + ses_suffix)] == [1]:
+        if new_dict[('ses-research' + ses_suffix)] == [1]:
             
             # set paths
             ses_path = subj_path / ('ses-research' + ses_suffix)
@@ -127,63 +130,74 @@ if __name__ == "__main__":
             if len(list(anat_path.glob('*fatsat*T2w*'))) > 0:
                 add_session_to_dict(
                     session=('ses-research' + ses_suffix + '_anat-t2fatsat'),
-                    subj_dict=subj_dict
+                    new_dict=new_dict,
+                    old_dict=old_dict
                 )
 
             if dwi_path.is_dir():
                 add_session_to_dict(
                     session=('ses-research' + ses_suffix + '_dwi'),
-                    subj_dict=subj_dict
+                    new_dict=new_dict,
+                    old_dict=old_dict
                 )
 
             if perf_path.is_dir():
                 add_session_to_dict(
                     session=('ses-research' + ses_suffix + '_perf'),
-                    subj_dict=subj_dict
+                    new_dict=new_dict,
+                    old_dict=old_dict
                 )
                 
             if len(list(func_path.glob('*resteyesopen*'))) > 0:
                 add_session_to_dict(
                     session=('ses-research' + ses_suffix + '_task-resteyesopen'),
-                    subj_dict=subj_dict
+                    new_dict=new_dict,
+                    old_dict=old_dict
                 )
                 
             if len(list(func_path.glob('*resteyesopen*physio*'))) > 0:
                 add_session_to_dict(
                     session=('ses-research' + ses_suffix + '_task-resteyesopen_physio'),
-                    subj_dict=subj_dict
+                    new_dict=new_dict,
+                    old_dict=old_dict
                 )
         
             if len(list(func_path.glob('*resteyesclosed*'))) > 0:
                 add_session_to_dict(
                     session=('ses-research' + ses_suffix + '_task-resteyesclosed'),
-                    subj_dict=subj_dict
+                    new_dict=new_dict,
+                    old_dict=old_dict
                 )
             
             if len(list(func_path.glob('*resteyesclosed*physio*'))) > 0:
                 add_session_to_dict(
                     session=('ses-research' + ses_suffix + '_task-resteyesclosed_physio'),
-                    subj_dict=subj_dict
+                    new_dict=new_dict,
+                    old_dict=old_dict
                 )
         
-        if subj_dict[('ses-meg' + ses_suffix)] == [1]:
+        # meg data will not occur postoperatively
+        if ses_suffix == '':
+            if new_dict[('ses-meg' + ses_suffix)] == [1]:
+                
+                ses_path = subj_path / ('ses-meg' + ses_suffix)
+                meg_path = ses_path / 'meg'
+                
+                if len(list(meg_path.glob('*resteyesopen*'))) > 0:
+                    add_session_to_dict(
+                        session=('ses-meg' + ses_suffix + '_task-resteyesopen'),
+                        new_dict=new_dict,
+                        old_dict=old_dict
+                    )
             
-            ses_path = subj_path / ('ses-meg' + ses_suffix)
-            meg_path = ses_path / 'meg'
-            
-            if len(list(meg_path.glob('*resteyesopen*'))) > 0:
-                add_session_to_dict(
-                    session=('ses-meg' + ses_suffix + '_task-resteyesopen'),
-                    subj_dict=subj_dict
-                )
+                if len(list(meg_path.glob('*resteyesclosed*'))) > 0:
+                    add_session_to_dict(
+                        session=('ses-meg' + ses_suffix + '_task-resteyesclosed'),
+                        new_dict=new_dict,
+                        old_dict=old_dict
+                    )
         
-            if len(list(meg_path.glob('*resteyesclosed*'))) > 0:
-                add_session_to_dict(
-                    session=('ses-meg' + ses_suffix + '_task-resteyesclosed'),
-                    subj_dict=subj_dict
-                )
-        
-        if subj_dict[('ses-altclinical' + ses_suffix)] == [1]:
+        if new_dict[('ses-altclinical' + ses_suffix)] == [1]:
             
             ses_path = subj_path / ('ses-altclinical' + ses_suffix)
             anat_path = ses_path / 'anat'
@@ -191,23 +205,26 @@ if __name__ == "__main__":
             if len(list(anat_path.glob('*T1w*'))) > 0:
                 add_session_to_dict(
                     session=('ses-altclinical' + ses_suffix + '_anat-t1'),
-                    subj_dict=subj_dict
+                    new_dict=new_dict,
+                    old_dict=old_dict
                 )
                 
             if len(list(anat_path.glob('*T2w*'))) > 0:
                 add_session_to_dict(
                     session=('ses-altclinical' + ses_suffix + '_anat-t2'),
-                    subj_dict=subj_dict
+                    new_dict=new_dict,
+                    old_dict=old_dict
                 )
                 
             if len(list(anat_path.glob('*FLAIR*'))) > 0:
                 add_session_to_dict(
                     session=('ses-altclinical' + ses_suffix + '_anat-flair'),
-                    subj_dict=subj_dict
+                    new_dict=new_dict,
+                    old_dict=old_dict
                 )
     
     # request user for missing values
-    for key, val in subj_dict.items():
+    for key, val in new_dict.items():
     
         if val == ['n/a']:
             new_val = input(
@@ -215,7 +232,7 @@ if __name__ == "__main__":
             )
 
             if new_val in val_options_dict[key][0]:
-                subj_dict[key] = [new_val]
+                new_dict[key] = [new_val]
                 print(
                     Colors.GREEN,
                     f"{key.capitalize()} set to '{new_val}' in participants.tsv for {pnum}",
@@ -239,7 +256,7 @@ if __name__ == "__main__":
     if not is_new_subject:
         df.drop(df.loc[df.participant_id == f'sub-{pnum}'].index, inplace=True)
     
-    append_df = pd.DataFrame(subj_dict)
+    append_df = pd.DataFrame(new_dict)
     out_df = pd.concat([df,append_df], ignore_index=True)
     
     out_df.sort_values(
